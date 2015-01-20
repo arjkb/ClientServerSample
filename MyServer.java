afc9f5ef0fb516ca5fa359b04ef5f4555bb1ed91
Arjun_Krishna_Babu <arjunkbabu@users.noreply.github.com>
2015-01-20 18:17:46 +0530

    Working server implementation

--- MyServer.java
+++ MyServer.java
@@ -5,13 +5,17 @@
 import java.net.*;
 import java.io.*;
 import java.util.*;
+
 public class MyServer {
-	public static void main(String args[]) throws IOException	{
-		final int PORT_ADDRESS = 3000;
+	public static void main(String args[]) throws IOException, FileNotFoundException	{
+		final int PORT_ADDRESS = 3001;
+		
+
+		String webpage;
 		
 		//create a socket which listens to port 3000
-		ServerSocket ss = new ServerSocket(3000);	
-		Socket soc = null;
+		ServerSocket ss = new ServerSocket(PORT_ADDRESS);	
+		Socket clientSocket = null;
 		
 		ss.setSoTimeout(30000);	
 		/*
@@ -21,22 +25,35 @@ public class MyServer {
 
 		try	{
 			//accepts a request from the client
-			soc = ss.accept();
+			clientSocket = ss.accept();
 
 			//attach the other end of the socket to a Scanner object
-			Scanner in = new Scanner( soc.getInputStream() );
+			Scanner in = new Scanner( clientSocket.getInputStream() );
+			PrintWriter out = new PrintWriter( clientSocket.getOutputStream(), true);
 
+			webpage = in.nextLine();
+			
+			FileReader fin = new FileReader(webpage);
+			BufferedReader br = new BufferedReader( fin );
+
+			String line = null;
+			while( (line = br.readLine()) != null )	{
+				out.println( line );
+			}
+
+/*
 			while( in.hasNextLine() )	{
 				String message = in.nextLine();	//retrieve the message...
 				System.out.print("\n SERVER: " + message); //print the message
 			}
+*/
 		}
 		catch(Exception E)	{
 			System.out.println("\n Oops! Something went wrong!");
 			E.printStackTrace();
 		}
 		finally	{
-			soc.close();
+			clientSocket.close();
 			ss.close();
 			System.out.println("\n Goodbye from server!");
 		}
